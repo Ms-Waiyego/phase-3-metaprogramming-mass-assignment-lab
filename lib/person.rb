328a01d0cd602824ace2ceebd8334c194e6c4701
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-10-31 17:36:16 +0300

    completed assignmnet

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
+  def initialize(attr)
+    attr.each do |key, val| 
+      self.class.attr_accessor(key)
+      self.send("#{key}=",val)
+    end 
+  end
+
+
 end
